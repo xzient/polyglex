f3e11e287638d0c283fe3d753a0dbee6fea8ebca
xzient <31088051+xzient@users.noreply.github.com>
2017-08-25 13:05:35 -0400

    Add empty singleton now
    Just added singleton user, nothing special about now.

---
+++ Polyglex/app/src/main/java/storeLocalData/UserSingleton.java
@@ -0,0 +1,11 @@
+package storeLocalData;
+
+/**
+ * Created by xavy_ on 8/25/2017.
+ */
+
+/**
+ * This singleton will to see if one is able to store data locally
+ */
+public class UserSingleton {
+}
